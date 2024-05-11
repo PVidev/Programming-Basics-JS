a7a766798b5ec504d00ecc0bf3e137745ddee29d
Peyo Videv <107483422+PVidev@users.noreply.github.com>
2024-05-11 12:54:41 +0300

    update For Loop - Lab

--- JS Basics/For Loop - Lab/07-sumOfNumbers.js
+++ JS Basics/For Loop - Lab/07-sumOfNumbers.js
@@ -1,5 +1,12 @@
 function sumOfNumbers(input) {
-    
+    let numAsString = String(input[0]);
+    let sum = 0;
+
+    for (let i = 0; i < numAsString.length; i++ ) {
+        let num = Number(numAsString[i]);
+        sum += num;
+    }
+    console.log(`The sum of the digits is:${sum}`);
 }
 
-sumOfNumbers([""]);
+sumOfNumbers(["1234"]);
