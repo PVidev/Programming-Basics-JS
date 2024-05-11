520ea4b5a68c1b3695ec6eecb7c3b7e6528974e2
Peyo Videv <107483422+PVidev@users.noreply.github.com>
2024-05-11 12:54:21 +0300

    update For Loop - Lab

--- JS Basics/For Loop - Lab/08-numbersDivisibleBy9.js
+++ JS Basics/For Loop - Lab/08-numbersDivisibleBy9.js
@@ -1,5 +1,23 @@
 function numbersDivisibleBy9(input) {
+    let startRange = Number(input[0]);
+    let endRange = Number(input[1]);
+    let sum = 0;
+    let buffer = "";
     
+    for (let i = startRange; i <= endRange; i++) {
+        if (i % 9 === 0) {
+            sum += i;
+            
+            if (i + 9 >= endRange) {
+                buffer += i;
+            } else {
+                buffer += i + "\n";
+            }
+        }
+    }
+    
+    console.log("The sum: " + sum);
+    console.log(buffer);
 }
 
-numbersDivisibleBy9([""]);
+numbersDivisibleBy9(["100", "200"]);
