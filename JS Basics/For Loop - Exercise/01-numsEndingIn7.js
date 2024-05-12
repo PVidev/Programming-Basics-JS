605ba8329e54730c4fafb08b4d0f224d5d91bc50
Peyo Videv <107483422+PVidev@users.noreply.github.com>
2024-05-12 09:38:37 +0000

    For Loop - Exercise

---
+++ JS Basics/For Loop - Exercise/01-numsEndingIn7.js
@@ -0,0 +1,7 @@
+function numsEndingIn7() {
+    for (let num = 7; num <= 997; num += 10) {
+            console.log(num);
+    }   
+}
+
+numsEndingIn7();
---
+++ JS Basics/For Loop - Exercise/08-tennisRanklist.js
@@ -0,0 +1,30 @@
+function tennisRanklist(input) {
+    let tournamentCount = Number(input[0]);
+    let startingPoints = Number(input[1]);
+
+    let seasonPoints = 0;
+    let winCount = 0;
+
+    for (let idx = 2; idx < input.length; idx++) {
+        let result = input[idx];
+
+        if (result === 'W') {
+            seasonPoints += 2000;
+            winCount++;
+        } else if (result === 'F') {
+            seasonPoints += 1200;
+        } else {
+            seasonPoints += 720;
+        }
+    }
+
+    console.log(`Final points: ${startingPoints + seasonPoints}`);
+
+    let avgPoints = seasonPoints / tournamentCount;
+    console.log(`Average points: ${Math.floor(avgPoints)}`);
+    
+    let percentWon = winCount / tournamentCount * 100;
+    console.log(`${percentWon.toFixed(2)}%`);
+}
+
+tennisRanklist(["5", "1400", "F", "SF", "W", "W", "SF"]);
